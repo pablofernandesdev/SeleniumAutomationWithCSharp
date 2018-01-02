2d2a27f3425ee499a797257b9bf95760576bd147
Pablo Fernandes <pablofsilva91@gmail.com>
2018-01-02 18:02:57 -0200

    First commit, Writing Test method using NUnit in Selenium C#

---
+++ SeleniumAutomation/SeleniumAutomation/Program.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation
+{
+    class Program
+    {
+        
+        IWebDriver driver = new ChromeDriver();
+        static void Main(string[] args)
+        {
+
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            
+            driver.Navigate().GoToUrl("http:\\www.google.com");
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys("executeautomation");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
+
+    }
+    
+}
